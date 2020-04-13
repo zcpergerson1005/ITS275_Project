3ff640d956a46d6ddd1bb52db3d88b585bc7a046
zcpergerson1005 <57502277+zcpergerson1005@users.noreply.github.com>
2020-04-13 01:30:03 -0400

    Add files via upload

---
+++ Customer1.java
@@ -0,0 +1,24 @@
+package finalexam;
+import java.util.Scanner;
+
+public class Customer1 extends Customers{
+	public Customer1(String customerName, String customerAddress, String customerZip, String customerState) {		//call upon the constructor of the superclass, Customer
+		super(customerName, customerAddress, customerZip, customerState); }		
+	@Override									//method overrides abstract method in superclass to get card information
+	public void getCard() {
+		System.out.println("Visa Debit Card: 4737 2892 3014 0224\n	Expires: 05/25\n	Security Number: 923");
+	}
+	
+	
+	public static void main(String[] args) {
+		 
+		Customer1 info1 = new Customer1("Mary Sue", "1321 Shell Lane", "49343", "NC");		//customer1 object(name, card info, street)
+		
+		System.out.println("Customer Database\n");				
+		System.out.println("Name: "+info1.getName() );
+		info1.getCard();
+		System.out.print("Street: "+info1.getAddress() );
+		System.out.print(", "+info1.getZip());
+		System.out.print(", "+info1.getState());
+	}
+}
---
+++ Customers.java
@@ -0,0 +1,44 @@
+package finalexam;
+import java.util.Scanner;
+
+public abstract class Customers extends Blink {	 
+	protected Customers(String customerName, String customerAddress, String customerZip, String customerState){ 		//protected so can still be used by subclasses
+		this.customerName = customerName;
+		this.customerAddress = customerAddress;
+		this.customerZip = customerZip;
+		this.customerState = customerState;
+	}
+	
+	static String customerName;
+	static String customerAddress;
+	static String customerZip;
+	static String customerState;
+	
+	private void setName(String name) {		//encapsulation- personal data should be private
+		String customerName = name;
+	}
+	public String getName() {		//get method to retrieve private data(name)
+		return customerName; 
+	}
+	private void setAddress(String address) {
+		String customerAddress = address;
+	}
+	public String getAddress() {
+		return customerAddress;				//method to retrieve address
+	}
+	
+	private void setZip(String zip) {
+		String customerZip = zip;
+	}
+	public String getZip() {
+		return customerZip;				//method to retrieve zip code
+	}	
+	public void setState(String state) {
+		String customerState = state;
+	}
+	public String getState() {
+		return customerState;
+	}
+	
+	public abstract void getCard(); 	//abstract method used to get card information
+}
